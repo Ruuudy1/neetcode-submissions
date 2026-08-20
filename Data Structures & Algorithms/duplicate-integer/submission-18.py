b38f95c9d533a23d69ad09bc194ae6ae03a43d47
Rudy Osuna <ruosuna@ucsd.edu>
2026-08-19 20:22:36 -0700

    Add: duplicate-integer - submission-18

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        counter = dict()
+        for i in range(len(nums)):
+            if nums[i] in counter:
+                return True 
+            counter[nums[i]] = counter.get(nums[i], 0) + 1
+        return False 

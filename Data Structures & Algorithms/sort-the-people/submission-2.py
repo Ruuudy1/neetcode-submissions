c63611edfae90fb79f8b43e3476ef8ac4463b7a4
Rudy Osuna <ruosuna@ucsd.edu>
2026-08-20 03:20:04 -0700

    Add: sort-the-people - submission-2

---
+++ Data Structures & Algorithms/sort-the-people/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # counter = dict()
+
+        arr = list(zip(heights, names))
+        arr.sort()
+        return [name for _, name in arr][::-1]
+
+
+        # for i in range(len(heights)):
+        #     counter[heights[i]] = names[i]
+        # return [value for key, value in sorted(counter.items(), reverse=True)]
